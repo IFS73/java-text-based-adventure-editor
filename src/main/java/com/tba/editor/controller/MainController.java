4d165841715cce4d372ac44150fbbda9416b6afc
Moonpearl <romain.tello@gmail.com>
2022-01-31 20:44:28 +0100

    Initial commit

---
+++ src/main/java/com/tba/editor/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.tba.editor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "/")
+public class MainController {
+
+    @GetMapping
+    public String home(Model model, @RequestParam(defaultValue = "World") String name) {
+        model.addAttribute("name", name);
+        return "home";
+    }
+}
